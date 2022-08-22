c84e47db5e8622b1a250082c0bebab64ba3dcd83
ibad81 <ibadbaig921@gmail.com>
2022-08-22 19:46:23 +0530

    brute force

---
+++ Code_forces/Games.java
@@ -0,0 +1,27 @@
+package Code_forces;
+
+import java.util.Scanner;
+
+public class Games {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        int arr1[]=new int[n];
+        int arr2[]=new int[n];
+        int count=0;
+
+        for(int i=0;i<n;i++){
+            arr1[i]=sc.nextInt();
+            arr2[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            for (int j = 0; j < n; j++) {
+                if(arr1[i]==arr2[j]){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
