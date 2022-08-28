ff80f74fb1c3e9c611b479544c37a8a10c483940
ibad81 <ibadbaig921@gmail.com>
2022-08-28 19:56:52 +0530

    MAths

---
+++ Code_forces/Candies_and_two_sisters.java
@@ -0,0 +1,19 @@
+package Code_forces;
+
+import java.util.Scanner;
+
+public class Candies_and_two_sisters {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int count=0;
+
+            
+            while(--n>=0){
+                int num=sc.nextInt();
+                System.out.println((n-1)/2);
+            }
+        
+        
+    }
+}
