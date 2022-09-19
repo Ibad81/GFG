20c31ac29cd2b19456514f6108cf75fab856d0ff
ibad81 <ibadbaig921@gmail.com>
2022-09-19 23:37:49 +0530

    Implementation , String

---
+++ Code_forces/Short_substrings.java
@@ -0,0 +1,23 @@
+package Code_forces;
+
+import java.util.Scanner;
+
+public class Short_substrings {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            String str=sc.next();
+            char[] cha=new char[str.length()];
+            for(int j=0;j<str.length();j++){
+                cha[j]=str.charAt(j);
+            }
+            System.out.print(cha[0]);
+            System.out.print(cha[1]);
+            for (int j = 3; j < cha.length; j=j+2) {
+                System.out.print(cha[j]);
+            }
+            System.out.println();
+        }
+    }
+}
